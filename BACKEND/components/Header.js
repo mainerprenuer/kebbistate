e045657b13e1eae254e0e54da12da37c4e65c576
Mousty Mainer <moustymainer@gmail.com>
2024-10-17 19:06:43 +0100

    added header props on admin

--- BACKEND/components/Header.js
+++ BACKEND/components/Header.js
@@ -1,9 +1,45 @@
+import { FaBarsStaggered } from "react-icons/fa6";
+import { BiExitFullscreen } from 'react-icons/bi'
+import { GoScreenFull } from 'react-icons/go'
+import { useState } from "react";
 
 
 export default function Header() {
    
-    return <>
+    const [isFullScreen, setIsFullscreen] = useState(false);
 
+    const toggleFullScreen = () => {
+        if(!document.fullscreenElement) {
+            document.documentElement.requestFullscreen().then(() => {
+                setIsFullscreen(true);
+            })
+        } else {
+            document.exitFullscreen().then(() => {
+                setIsFullscreen(false);
+            })
+        }
+    }
+
+    return <>
+        <header className="header flex flex-sb">
+            <div className="logo flex gap-2">
+                <h1>ADMIN</h1>
+                <div className="headerham flex flex-center">
+                <FaBarsStaggered />
+                </div>
+            </div>
+            <div className="rightnav flex gap-2">
+                <div onClick={toggleFullScreen}>
+                    {isFullScreen ? <BiExitFullscreen /> : <GoScreenFull />}
+                </div>
+                <div className="notification">
+                    <img src="/img/notification.png" alt="notification"/>
+                </div>
+                <div className="profilenav">
+                    <img src="/img/user.png" alt="user"/>
+                </div>
+            </div>
+        </header>
    
     </>
 }
