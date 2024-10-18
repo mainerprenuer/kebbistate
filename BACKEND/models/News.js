418e5c7353b903b84d29e727b44b97907e4fdf61
Mousty Mainer <moustymainer@gmail.com>
2024-10-18 19:41:54 +0100

    populate news models to db.

---
+++ BACKEND/models/News.js
@@ -0,0 +1,18 @@
+import { mode } from '@cloudinary/url-gen/actions/rotate';
+
+const { Schema, models, model } = require('mongoose');
+
+const NewsSchema = new Schema({
+    title: { type: String, required: true },
+    slug: { type: String, required: true },
+    images: [{ type: String }],
+    description: [{ type: String }],
+    newscategory: [{ type: String }],
+    tags: [{ type: String }],
+    status: [{ type: String }],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+}, {
+    timestamps: true, 
+});
+
+export const News = models.News || model('News', NewsSchema, 'news');
